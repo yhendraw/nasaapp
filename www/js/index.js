f8783b186c966c62c5df01ae7dcde7fa1d086849
yuliana hendra w <27959213+yhendraw@users.noreply.github.com>
2018-06-03 21:38:46 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=https://api.nasa.gov/planetary/apod?api_key=c2ibj1U6icyPo7cT7ytiQlUy06ApwtS4e2fuTvHl";
         
         $("#loadImage").click(function(){
             $.ajax({
